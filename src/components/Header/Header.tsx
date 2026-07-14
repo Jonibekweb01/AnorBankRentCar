323020b30bf9d5dfd17d470768e15787a0367c7b
Jonibek <jonibekqodirovv@gmail.com>
2026-07-14 10:37:34 +0500

    rename logo asset to lowercase

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import { Container } from '@mantine/core'
 
-import Logo from '../../assets/images/logo.svg'
+import Logo from '../../assets/images/Logo.svg'
 import Call from '../../assets/images/call-icon.svg'
 
 import {
